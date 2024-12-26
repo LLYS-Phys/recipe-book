47987fa8af28a3ec7ebeb78032495f026db0a1a4
LLYS-Phys <lachyordanov@gmail.com>
2024-12-26 19:17:09 +0200

    improve styles for different image dimensions

--- src/app/recipies-list/recipies-list.component.ts
+++ src/app/recipies-list/recipies-list.component.ts
@@ -4,11 +4,12 @@ import { Observable } from 'rxjs';
 import { Recipe } from '../recipe.model';
 import { RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-recipies-list',
   standalone: true,
-  imports: [RouterLink, CommonModule],
+  imports: [RouterLink, CommonModule, MatButtonModule],
   templateUrl: './recipies-list.component.html',
   styleUrl: './recipies-list.component.scss'
 })
@@ -16,19 +17,59 @@ export class RecipiesListComponent implements OnInit {
   constructor(private http: HttpClient, private destroyRef: DestroyRef) {}
   recipes: Recipe[] = []
   recipesLoaded: boolean = false
+  categories: string[] = []
+  filters: string[] = []
 
   private fetchRecipes() {
     return this.http.get<Observable<Recipe>>('https://recipe-book-406c3-default-rtdb.europe-west1.firebasedatabase.app/recipes.json')
   }
 
   ngOnInit() {
+    this.getAllRecipies()
+  }
+
+  private getAllRecipies() {
     const recipesSubscription = this.fetchRecipes()
-      .subscribe({
-        next: (data) => Object.values(data).forEach((recipe: Recipe) => this.recipes.push(recipe)),
-        complete: () => this.recipesLoaded = true,
-        error: (err) => console.log(err)
-      })
+    .subscribe({
+      next: (data) => Object.values(data).forEach((recipe: Recipe) => this.recipes.push(recipe)),
+      complete: () => {
+        this.recipesLoaded = true
+        this.recipes.forEach((recipe) => {
+          recipe.categories.forEach((category) => {
+            if (!this.categories.includes(category)) this.categories.push(category)
+          })
+        })
+      },
+      error: (err) => console.log(err)
+    })
 
     this.destroyRef.onDestroy(() => recipesSubscription.unsubscribe())
+  }
+
+  private getFilteredRecipies(filters: string[]) {
+    const recipesSubscription = this.fetchRecipes()
+    .subscribe({
+      next: (data) => Object.values(data).forEach((recipe: Recipe) => {
+        if (filters.every(i => recipe.categories.includes(i))) this.recipes.push(recipe)
+      }),
+      complete: () => {
+        this.recipesLoaded = true
+        this.recipes.forEach((recipe) => {
+          recipe.categories.forEach((category) => {
+            if (!this.categories.includes(category)) this.categories.push(category)
+          })
+        })
+      },
+      error: (err) => console.log(err)
+    })
+
+    this.destroyRef.onDestroy(() => recipesSubscription.unsubscribe())
+  }
+
+  applyFilter(category: string){
+    this.recipes = []
+    this.recipesLoaded = false
+    this.filters.includes(category) ? this.filters = this.filters.filter(item => item !== category) : this.filters.push(category);
+    this.filters.length == 0 ? this.getAllRecipies() : this.getFilteredRecipies(this.filters)
   }
 }
