752d441bc492e0afbfe4efbdf6c3159fa68b18c1
LLYS-Phys <lachyordanov@gmail.com>
2025-02-06 15:12:31 +0200

    add query parameters handling

--- src/app/recipies-list/recipies-list.component.ts
+++ src/app/recipies-list/recipies-list.component.ts
@@ -1,10 +1,11 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, DestroyRef, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, take } from 'rxjs';
 import { Recipe } from '../recipe.model';
-import { RouterLink } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-recipies-list',
@@ -14,13 +15,16 @@ import { MatButtonModule } from '@angular/material/button';
   styleUrl: './recipies-list.component.scss'
 })
 export class RecipiesListComponent implements OnInit {
-  constructor(private http: HttpClient, private destroyRef: DestroyRef) {}
+  constructor(private http: HttpClient, private destroyRef: DestroyRef, private router: Router, private activatedRoute: ActivatedRoute, private location: Location) {}
   recipes: Recipe[] = []
   recipesLoaded: boolean = false
-  categories: string[] = []
+  categories_key: string[] = []
+  categories_wording: string[] = []
+  categories: {key: string, wording: string}[] = []
   filters: string[] = []
   locale: string | null = null
-  initialRecipes: Observable<Observable<Recipe>> | null = null
+  initialRecipes: Recipe[] = []
+  tempFilter: string | null = null
 
   private fetchRecipes() {
     return this.http.get<Observable<Recipe>>('https://recipe-book-406c3-default-rtdb.europe-west1.firebasedatabase.app/recipes.json')
@@ -28,84 +32,89 @@ export class RecipiesListComponent implements OnInit {
 
   ngOnInit() {
     this.locale = localStorage.getItem('locale')
-    document.querySelector("header")?.addEventListener("click", () => {
+    document.querySelector(".navigation")?.addEventListener("click", () => {
       this.locale = localStorage.getItem('locale')
-      this.recipes = []
       this.recipesLoaded = false
-      this.getAllRecipies()
+      this.setParameters()
     })
     document.querySelector(".mobile-menu-list")?.addEventListener("click", () => {
       this.locale = localStorage.getItem('locale')
-      this.recipes = []
       this.recipesLoaded = false
-      this.getAllRecipies()
+      this.setParameters()
     })
-    this.initialRecipes = this.fetchRecipes()
-    this.getAllRecipies()
+    document.querySelector(".home-link")?.addEventListener("click", () => {
+      this.recipesLoaded = false
+      this.filters = []
+      this.setParameters()
+    })
+    const recipesSubscription = this.fetchRecipes().pipe(take(1)).subscribe({
+      next: (data) => Object.values(data).forEach((recipe: Recipe) => this.initialRecipes!.push(recipe)),
+      complete: () => {
+        this.activatedRoute.queryParams.pipe(take(1)).subscribe({
+          next: (data) => {
+            this.tempFilter = data['filter']
+          },
+          complete: () => {
+            this.initialRecipes.forEach((recipe) => {
+              recipe.categories.forEach((category) => {
+                // Only check if the category.key is already in the categories array
+                if (!this.categories.some(item => item.key === category)) {
+                  this.categories.push({ key: category, wording: category });
+                }
+              });
+            
+              // Update the wording only if we're in the 'bg' locale
+              if (this.locale === 'bg') {
+                this.categories.forEach((category) => {
+                  const index = recipe.categories.indexOf(category.key);
+                  if (index !== -1) category.wording = recipe.categories_bg[index];
+                });
+              }
+            })
+            if (this.tempFilter) {
+              this.filters = this.tempFilter!.split(",")
+            }
+            this.categories = this.categories.sort((a, b) => {
+              const wordingA = a.wording.toLowerCase();
+              const wordingB = b.wording.toLowerCase();
+              if (wordingA < wordingB) return -1;
+              if (wordingA > wordingB) return 1;
+              return 0;
+            });
+            this.setParameters()
+          }
+        }); 
+      }
+    })
+
+    this.destroyRef.onDestroy(() => recipesSubscription.unsubscribe())
   }
 
   private getAllRecipies() {
-    const recipesSubscription = this.initialRecipes!
-    .subscribe({
-      next: (data) => Object.values(data).forEach((recipe: Recipe) => this.recipes.push(recipe)),
-      complete: () => {
-        this.categories = []
-        this.recipesLoaded = true
-        this.recipes.forEach((recipe) => {
-          if (this.locale == 'bg'){
-            recipe.categories_bg.forEach((category) => {
-              if (!this.categories.includes(category)) this.categories.push(category)
-            })
-          }
-          else{
-            recipe.categories.forEach((category) => {
-              if (!this.categories.includes(category)) this.categories.push(category)
-            })
-          }
-        })
-      },
-      error: (err) => console.log(err)
-    })
-
-    this.destroyRef.onDestroy(() => recipesSubscription.unsubscribe())
+    this.recipes = []
+    Object.values(this.initialRecipes!).forEach((recipe: Recipe) => this.recipes.push(recipe))
+    this.recipesLoaded = true
   }
 
   private getFilteredRecipies(filters: string[]) {
-    const recipesSubscription = this.initialRecipes!
-    .subscribe({
-      next: (data) => Object.values(data).forEach((recipe: Recipe) => {
-        if (this.locale == 'bg'){
-          if (filters.every(i => recipe.categories_bg.includes(i))) this.recipes.push(recipe)
-        }
-        else{
-          if (filters.every(i => recipe.categories.includes(i))) this.recipes.push(recipe)
-        }
-      }),
-      complete: () => {
-        this.recipesLoaded = true
-        this.recipes.forEach((recipe) => {
-          if (this.locale == 'bg'){
-            recipe.categories_bg.forEach((category) => {
-              if (!this.categories.includes(category)) this.categories.push(category)
-            })
-          }
-          else{
-            recipe.categories.forEach((category) => {
-              if (!this.categories.includes(category)) this.categories.push(category)
-            })
-          }
-        })
-      },
-      error: (err) => console.log(err)
-    })
-
-    this.destroyRef.onDestroy(() => recipesSubscription.unsubscribe())
+    this.recipes = []
+    Object.values(this.initialRecipes!).forEach((recipe: Recipe) => { if (filters.every(i => recipe.categories.includes(i))) this.recipes.push(recipe) })
+    this.recipesLoaded = true
   }
 
   applyFilter(category: string){
-    this.recipes = []
-    this.recipesLoaded = false
     this.filters.includes(category) ? this.filters = this.filters.filter(item => item !== category) : this.filters.push(category);
+    if (this.filters.length > 0) {
+      this.router.navigate([''], { queryParams: { filter: this.filters.join(',') } });
+    }
+    else{
+      this.router.navigate([''], { queryParams: { } }); 
+    }
+    this.setParameters()
+  }
+
+  setParameters() {
+    this.recipesLoaded = false
     this.filters.length == 0 ? this.getAllRecipies() : this.getFilteredRecipies(this.filters)
   }
 }
